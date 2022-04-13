22bc69e7c41f32ce468128b8021fa0fbbf1d8e0e
RebekkaMa <rebekka@martens-sachsen.de>
2022-04-13 16:31:10 +0200

    Next Step

--- src/commonMain/kotlin/Grammar.kt
+++ src/commonMain/kotlin/Grammar.kt
@@ -18,33 +18,34 @@ class Grammar(rules: ArrayList<Rule>) {
     }
 
     fun getLexicon(): List<String> {
-        return pRules.filterKeys { it.lexical }.map { (rule, p) -> rule.lhs + " " + rule.rhs + " " + (getWholeNumberOrNull(p)?: getRoundetNumber(p, 22)) }
+        return pRules.filterKeys { it.lexical }.map { (rule, p) -> rule.lhs + " " + rule.rhs + " " + (p.getRoundetNumber(22)) }
     }
 
     fun getRules(): List<String> {
-        return pRules.filterKeys { !it.lexical }.map { (rule, p) -> rule.lhs + " -> " + rule.rhs + " " + (getWholeNumberOrNull(p)?: getRoundetNumber(double = p, 22) )}
+        return pRules.filterKeys { !it.lexical }.map { (rule, p) -> rule.lhs + " -> " + rule.rhs + " " + (p.getRoundetNumber(22) )}
     }
 
     override fun toString(): String {
         return "Initial: " + initial + "\n" + "Rules: \n" + pRules.map { (rule, p) -> "$rule    $p" }.joinToString("\n")
     }
 
-    private fun getWholeNumberOrNull(double: Double): Int? {
-        val intValue = double.toInt()
-        if (double.rem(intValue) == 0.0) return intValue
-        return null
-    }
+//    private fun getWholeNumberOrNull(double: Double): Int? {
+//        val intValue = double.toInt()
+//        if (double.rem(intValue) == 0.0) return intValue
+//        return null
+//    }
+}
 
-    private fun getRoundetNumber(double: Double, backNumber: Int): String {
-        val beforeDecimalPointRange = double.toInt()
-        var afterDecimalPointDouble = double - beforeDecimalPointRange
-        var afterDecimalPointRange = ""
-        for (i in 1..backNumber){
-            afterDecimalPointDouble = afterDecimalPointDouble.times(10)
-            afterDecimalPointRange += afterDecimalPointDouble.toInt().toString()
-            afterDecimalPointDouble -= afterDecimalPointDouble.toInt()
-        }
-        afterDecimalPointRange += afterDecimalPointDouble.times(10).roundToInt().toString()
-        return "$beforeDecimalPointRange.$afterDecimalPointRange"
+fun Double.getRoundetNumber(backNumber: Int): String {
+    val beforeDecimalPointRange = this.toInt()
+    var afterDecimalPointDouble = this - beforeDecimalPointRange
+    var afterDecimalPointRange = ""
+    for (i in 1 until backNumber){
+        afterDecimalPointDouble = afterDecimalPointDouble.times(10)
+        afterDecimalPointRange += afterDecimalPointDouble.toInt().toString()
+        afterDecimalPointDouble -= afterDecimalPointDouble.toInt()
     }
+    afterDecimalPointRange += afterDecimalPointDouble.times(10).roundToInt().toString()
+
+    return "$beforeDecimalPointRange.$afterDecimalPointRange".replace("\\.?(0+)$".toRegex(), "")
 }
--- src/commonTest/kotlin/GrammarTest.kt
+++ src/commonTest/kotlin/GrammarTest.kt
@@ -20,14 +20,40 @@ class GrammarTest {
 
         val grammar = Grammar(arrayListOf(rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10))
         grammar.pRules shouldBe mapOf(
-            (Rule(false, "S", "NP VP") to 1f),
-            (Rule(true, "NP", "John") to 1/3f),
-            (Rule(false, "VP", "V NP") to 1f),
-            (Rule(true, "V", "hit") to 1f),
-            (Rule(false, "NP", "DET N") to 2/3f),
-            (Rule(true, "DET", "the") to 1f),
-            (Rule(true, "N", "ball") to 1/2f),
-            (Rule(true, "N", "ground") to 1/2f)
+            (Rule(false, "S", "NP VP") to 1.0),
+            (Rule(true, "NP", "John") to 1/3.toDouble()),
+            (Rule(false, "VP", "V NP") to 1.0),
+            (Rule(true, "V", "hit") to 1.0),
+            (Rule(false, "NP", "DET N") to 2/3.toDouble()),
+            (Rule(true, "DET", "the") to 1.0),
+            (Rule(true, "N", "ball") to 1/2.toDouble()),
+            (Rule(true, "N", "ground") to 1/2.toDouble())
         )
     }
+
+
+    @Test
+    fun getRoundedNumber_shouldReturnTheSameNumberString() = runTest {
+
+        val double = 0.04
+        double.getRoundetNumber(10) shouldBe "0.04"
+
+    }
+
+    @Test
+    fun getRoundedNumber_shouldReturnTheRoundedNumberString() = runTest {
+
+        val double = 0.00123456789191
+        double.getRoundetNumber(10) shouldBe "0.0012345679"
+
+    }
+
+    @Test
+    fun getRoundedNumber_shouldReturnTheNumberWithoutPointString() = runTest {
+
+        0.0.getRoundetNumber(10) shouldBe "0"
+        1.0.getRoundetNumber(10) shouldBe "1"
+
+    }
+
 }
