0255a34920a5145f82be2a978722afd2fcfaff58
RebekkaMa <rebekka@martens-sachsen.de>
2022-05-11 13:11:40 +0200

    Aufgabe 2: Erste Version

--- src/main/kotlin/model/Backtrace.kt
+++ src/main/kotlin/model/Backtrace.kt
@@ -1,17 +1,14 @@
 package model
 
-data class Backtrace(val bin: Pair<Rule, Double>, val chain: Pair<Backtrace?, Backtrace?>?) {
+data class Backtrace(val bin: Pair<Rule, Double>, val chain: Pair<Backtrace, Backtrace?>?) {
 
     fun getParseTreeAsString(): String {
-        fun getPartOfTree(bt : Backtrace?): String {
-            if (bt == null) return ""
-            if (bt.chain == null) {
-                return "(" + bt.bin.first.lhs + " " + bt.bin.first.rhs.first() + ")"
+        fun getPartOfTree(bt : Backtrace): String {
+            return when {
+                bt.chain == null -> "(" + bt.bin.first.lhs + " " + bt.bin.first.rhs.first() + ")"
+                bt.chain.second == null -> "(" + bt.bin.first.lhs + " " + getPartOfTree(bt.chain.first) + ")"
+                else -> "(" + bt.bin.first.lhs + " " + getPartOfTree(bt.chain.first) + " " + getPartOfTree(bt.chain.second!!) + ")"
             }
-            if (bt.chain.second == null) {
-                return "(" + bt.bin.first.lhs + " " + getPartOfTree(bt.chain.first) + ")"
-            }
-            return "(" + bt.bin.first.lhs + " " + getPartOfTree(bt.chain.first) + " " + getPartOfTree(bt.chain.second) + ")"
         }
         return getPartOfTree(this)
     }
